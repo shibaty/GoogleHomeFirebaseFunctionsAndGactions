74d74bd20780d37eeec141b7a4357941e28e6d72
shibaty <shibaty@gmail.com>
2018-01-30 02:27:31 +0900

    add pm2 config

---
+++ gactions/ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps : [
+
+    {
+      name      : 'GactionsTestHeartBeat',
+      script    : 'lib/index.js',
+      log_date_format: "YYYY-MM-DD HH:mm:SS"
+    }
+
+  ]
+};
